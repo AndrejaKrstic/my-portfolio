f6be995aa16692aac19b1311911c9d12fc8cba3f
AndrejaKrstic <andrejakrstic5121@gmail.com>
2024-10-28 14:30:48 +0100

    add the sugar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["demo-source.imgix.net"], // Add your Imgix hostname here
+  },
 };
 
 export default nextConfig;
